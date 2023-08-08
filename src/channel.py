b546f04595c6657275f5c534fc21b7dd43fa3fa6
daryaagafonova <ADashyta2013@yandex.ru>
2023-08-08 19:37:16 +0300

    Modify the Channel constructor, new attributes are added. Added methods to the Channel class: get_service - returns an object for working with thw YouTube API. to_json method - saves the values of the Channel instance attributes to a file.

--- src/channel.py
+++ src/channel.py
@@ -13,11 +13,17 @@ class Channel:
 
     """Класс для ютуб-канала"""
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id: str, title: str, description: str, url: str, count_subscribers: int, video_count: int, all_count_views: int) -> None:
 
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
 
         self.channel_id = channel_id
+        self.title = None
+        self.description = None
+        self.url = None
+        self.count_subscribers= None
+        self.video_count = None
+        self.all_count_views = None
 
 
     def print_info(self) -> None:
@@ -26,4 +32,31 @@ class Channel:
 
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
-        print (json.dumps(channel, indent=2, ensure_ascii=False))
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+
+    @classmethod
+    def get_service(cls):
+
+        """ Возвращает объект для работы с YouTube API """
+
+        return youtube
+
+
+    def to_json(self):
+
+        """ Метод, сохраняющий в файл значения атрибутов экземпляра Channel """
+
+        data = {
+                "id": self.channel_id,
+                "title": self.title,
+                "description": self.description,
+                "url": self.url,
+                "count_subscribers": self.count_subscribers,
+                "video_count": self.video_count,
+                "all_count_views": self.all_count_views
+        }
+
+        with open("", "w") as json_file:
+
+            json.dump(data, json_file, indent=2, ensure_ascii=False)
